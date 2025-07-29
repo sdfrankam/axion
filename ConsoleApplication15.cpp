09725a2dfb9baa84558a9ee02628e54179983a85
sdfrankam <sdfrankam@gmail.com>
2025-07-29 16:01:06 +0300

    Add files via upload

---
+++ ConsoleApplication15.cpp
@@ -0,0 +1,54 @@
+﻿// ConsoleApplication15.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double L_4 = pow(0.05, 4);
+    ifstream csv_y, csv_z;
+    ofstream rho_X, rho_V;
+    csv_y.open("teta_q.txt");
+    csv_z.open("dteta_q.txt");
+    rho_X.open("rho_x.txt");
+    rho_V.open("rho_v.txt");
+    string line1, line2;
+    int i = 0;
+    double r0;
+    while (getline(csv_z, line1) && getline(csv_y, line2)) {
+        double z = stod(line1);
+        double y = stod(line2);
+        //КВИНТЕССЕНЦИЯ
+        //r0 = z * z / 2 + pow(0.05, 4) * (1 - cos(y));
+        //rho_X << ( z * z / (2.0*r0)) << endl;
+        //rho_V << pow(0.05, 4) * (1 - cos(y))/r0 << endl;
+
+        //ТАХИОНЫ
+        //double L_4 = pow(0.05, 4);
+        //r0 = L_4 * (1 - cos(y)) * (1+L_4*z*z/ (2*(1 - L_4 * z * z)));
+        //rho_X << (L_4*L_4*z*z*(1-cos(y))/(2*(1-L_4*z*z)))/r0 << endl;
+        //rho_V << L_4 *(1-cos(y))/ r0 << endl;
+
+        //ФАНТОМНЫЕ ПОЛЯ
+        //r0 = -z * z / 2 + pow(0.05, 4) * (1 - cos(y));
+        //rho_X << ( -z * z / (2.0*r0)) << endl;
+        //rho_V << pow(0.05, 4) * (1 - cos(y))/r0 << endl;
+
+        //a>>1, b=0
+        double a = 5.0;
+        r0 = (2*a*a*a-a)*pow(z*z/2.0, a*a)+L_4*(1-cos(y));
+        rho_X << (2 * a * a * a - a) * pow(z * z / 2.0, a * a)/r0 << endl;
+        rho_V << L_4 *(1-cos(y))/ r0 << endl;
+
+
+    }
+    csv_y.close();
+    csv_z.close();
+    rho_X.close();
+    rho_V.close();
+}
+
---
+++ ConsoleApplication19.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <typeinfo>
+#include <malloc.h>
+#include "Header.h"
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Print size of array : " << endl;
+    cin >> n;
+    float* buffer_array = new float[n];
+    for (int i = 0; i < n; i++) {
+        float buffer_el;
+        cout << "Print element of array n. " << i + 1 << endl;
+        cin >> buffer_el;
+        buffer_array[i] = buffer_el;
+
+    }
+    
+    SimpleFloatArray new_arr(buffer_array, n);
+    new_arr.setSize(7);
+    cout << "size of array increase to 7 : " << endl;
+    for (int i = 0; i < new_arr.numElems(); i++) {
+        cout << new_arr[i] << " ";
+    };
+    cout << endl;
+    new_arr.setSize(3);
+    cout << "size of array decrease to 3 : " << endl;
+    for (int i = 0; i < new_arr.numElems(); i++) {
+        cout << new_arr[i] << " ";
+    };
+    cout << endl;
+    new_arr.setSize(7);
+    cout << "size of array increase to 7 again : " << endl;
+    for (int i = 0; i < new_arr.numElems(); i++) {
+        cout << new_arr[i] << " ";
+    };
+    cout << endl;
+    cout << "numElems function " << new_arr.numElems() << endl;
+
+    //new_arr.~SimpleFloatArray();
+
+}
